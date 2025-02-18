b82cbb52c597224d7275bbfd9132f9d8d0796979
Bourhlef-Y <bourhlef.yacine53@gmail.com>
2025-02-18 16:04:53 +0100

    remove readme, correction of task 0

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module contenant des fonctions pour filtrer les données sensibles dans les logs
+"""
 import re
+from typing import List
 
-def filter_datum(fields, redaction, message, separator):
-    """Remplace les valeurs des champs spécifiés par une chaîne de redaction"""
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """Remplace les valeurs des champs sensibles par une valeur de redaction"""
     pattern = f'({"|".join(fields)})=[^{separator}]*'
     return re.sub(pattern, f'\\1={redaction}', message)
