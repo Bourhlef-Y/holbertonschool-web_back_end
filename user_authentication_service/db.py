110d0b420f12185b08aca0f9b6140b43c9abeb73
Bourhlef-Y <bourhlef.yacine53@gmail.com>
2025-02-28 10:33:06 +0100

    add task 1 to adding a user to the database

---
+++ user_authentication_service/db.py
@@ -0,0 +1,45 @@
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Ajoute un nouvel utilisateur à la base de données
+
+        Args:
+            email: L'email de l'utilisateur
+            hashed_password: Le mot de passe hashé de l'utilisateur
+
+        Returns:
+            User: L'objet User créé
+        """
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
