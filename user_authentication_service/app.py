6d9a35fb98920e870f6a1d3989654123edef2b56
Bourhlef-Y <bourhlef.yacine53@gmail.com>
2025-02-28 10:59:48 +0100

    task 7

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python3
 """Module de l'application Flask
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
 def welcome():
     """Route GET racine qui retourne un message de bienvenue"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users():
+    """Route POST pour enregistrer un nouvel utilisateur"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
