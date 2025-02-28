43c98c502ec2e418a725897c1808c6d1342e232e
Bourhlef-Y <bourhlef.yacine53@gmail.com>
2025-02-28 10:56:53 +0100

    add task 5

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -24,3 +24,33 @@ def _hash_password(password: str) -> bytes:
     hashed = bcrypt.hashpw(password_bytes, salt)
 
     return hashed
+
+
+class Auth:
+    """Classe Auth pour interagir avec la base de données d'authentification
+    """
+
+    def __init__(self):
+        """Initialise une nouvelle instance Auth"""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Enregistre un nouvel utilisateur dans la base de données
+
+        Args:
+            email: Email de l'utilisateur
+            password: Mot de passe de l'utilisateur
+
+        Returns:
+            User: L'utilisateur créé
+
+        Raises:
+            ValueError: Si l'utilisateur existe déjà
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
